e6696d81fcb0ab236a4eeaf60d9b731a1d0e7298
Matthew Clark <mclark@localhost.localdomain>
2020-02-25 08:15:31 -0500

    update with comments

--- createdb.py
+++ createdb.py
@@ -1,10 +1,15 @@
-
+#
+# impott the set of RMC files into a postgres database.
+# this assumes that the current directory is the one with the file set.
+#
+# TODO: enhance so one does not have to unzip the files first
+#
 import readrmcfiles as rmc
 
 
-#rmc.readassay()
-#rmc.readcitation()
-#rmc.readdatapoint()
-#rmc.readfact()
+rmc.readassay()
+rmc.readcitation()
+rmc.readdatapoint()
+rmc.readfact()
 rmc.readtarget()
-#rmc.readsdfile()
+rmc.readsdfile()
