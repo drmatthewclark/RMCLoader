a556ae7a29c1f967b58189ce2ee73465f6f99a71
Matthew Clark <mclark@drmatthewclark.com>
2021-07-22 14:15:51 -0400

    add indexing

--- readrmcfiles.py
+++ readrmcfiles.py
@@ -1,18 +1,27 @@
-# rmc file release number
-rmcversion = '200171'
+#
+# this is designed to run from the RMC version directory, e.g. 201857.
+# it further assumes that the loader information is in a directory ../loader from
+# that directory
+
+# owner of database or db to connect to
 dbname='mclark'
 debug = False
+CHUNKSIZE = 100000
 
 import xml.etree.ElementTree as ET
 import psycopg2 as psql
 from   psycopg2 import sql
-from psycopg2.extensions import AsIs
+from   psycopg2.extensions import AsIs
 import glob
 import gzip
+from   rdkit import Chem
+from   rdkit import RDLogger
 
-# for rdkit Mol to Smiles
-from rdkit import Chem
-from rdkit import RDLogger
+# global db of hash of all lines added to databae
+lines = set()
+
+# shared cache of sql to execute
+insertcache = set()
 
 def readfile(fname, key, dbname, sql):
     """ 
@@ -67,76 +76,79 @@ def readfile(fname, key, dbname, sql):
                     newlist.append(value)
                     data[tag] = newlist 
 
-       columns = data.keys()
-       values = [data[column] for column in columns]
-       if debug:
-           print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))
-       else:
-           cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+       writedb(conn, data, sql)
+
+    flush(conn) 
+    conn.close()
+    print("\t%i records" % (len(lines)))
+
+
+def sqlfromfile(schemafile):
+
+    conn = psql.connect(user=dbname)
+    with open(schemafile, 'r') as schema:
+        f = schema.read()
+        with conn.cursor() as cur:
+            cur.execute(f)
+            print('created schema from', fname)
 
     conn.commit()
     conn.close()
 
+def initdb(conn):
 
-def delete(tablename):
-    """ clean out table for re-import """
-    if debug:
-        return
-
-    conn=psql.connect(user=dbname)
+    conn = psql.connect(user=dbname)
+    """ initialize the database"""
+    drop  = "drop schema rmc cascade;"
+    schemafile = '../loader/rmc.schema'
     with conn.cursor() as cur:
-        print('deleting records from', tablename)
-        query = 'delete from ' + tablename
-        # this is a criminal statmenet but the query parameter replacement
-        # syntax doesn't work for table names, only data elements.
-        cur.execute(query)
-    conn.commit()
-    conn.close()
+      try :
+        cur.execute(drop)
+        print("dropped existing schema")
+      except:
+        print("failed to drop schema")
+        exit(5)
 
+    conn.commit()
+    sqlfromfile(conn, schemafile)
+    conn.close()
    
 
 def readassay():
   """ read the assay data files into the assay table """
 
-  delete('rmc.assay')
-  sql = 'insert into rmc.assay (%s) values %s'
-  for filepath in glob.iglob('./' + rmcversion + '_assays_*.xml.gz'):
+  sql = 'insert into rmc.assay (%s) values %s;'
+  for filepath in glob.iglob('./*_assays_*.xml.gz'):
     readfile(filepath, 'aid', dbname, sql)
 
 
 def readcitation():
   """ read the citation files into the citation table """
-  delete('rmc.citation')
-  sql = 'insert into rmc.citation (%s) values %s'
-  for filepath in glob.iglob('./' + rmcversion + '_citations_*.xml.gz'):
+  sql = 'insert into rmc.citation (%s) values %s;'
+  for filepath in glob.iglob('./*_citations_*.xml.gz'):
     readfile(filepath, 'cid', dbname, sql)
 
 
 def readdatapoint():
   """ read the datapoint files into the datapoint table """
 
-  delete('rmc.datapoint')
-  sql = 'insert into rmc.datapoint (%s) values %s'
-  for filepath in glob.iglob('./' + rmcversion + '_datapoints_*.xml.gz'):
+  sql = 'insert into rmc.datapoint (%s) values %s;'
+  for filepath in glob.iglob( './*_datapoints_*.xml.gz'):
     readfile(filepath, 'did', dbname, sql)
 
 
 def readfact():
   """ read the facts file into the fact table """
-  delete('rmc.fact')
-  sql = 'insert into rmc.fact (%s) values %s'
-  for filepath in glob.iglob('./' + rmcversion + '_facts_*.xml.gz'):
+  sql = 'insert into rmc.fact (%s) values %s;'
+  for filepath in glob.iglob('./*_facts_*.xml.gz'):
     readfile(filepath, 'rxid', dbname, sql)
 
 
 def readtarget():
     """ read the targets file into the target table
-    this requires a special function because the XML is nested two
-    levels deep
     """
-    delete('rmc.target')
-    sql = 'insert into rmc.target (%s) values %s'
-    for filepath in glob.iglob('./' + rmcversion + '_targets*.xml.gz'):
+    sql = 'insert into rmc.target (%s) values %s;'
+    for filepath in glob.iglob('./*_targets*.xml.gz'):
         readfile(filepath, 'tid', dbname, sql)
 
 nl = '\n'  # newline; cound be \r\n if necessary
@@ -177,6 +189,7 @@ def readnextSDfile(file):
     # one can add the sdfile too if you want
     smiles = ''
 
+    # one particular file seems to be problematic
     if tags is not None and 'XRN' in tags.keys() and tags['XRN'] == '21291617':
         sdfile = ''   
       
@@ -198,6 +211,7 @@ def readnextSDfile(file):
 def readsdfiles(fname):
     """ read all of the individual SDFiles from the concatenated SDFile """
     print('readsdfile ', fname)
+    sql = 'insert into rmc.sdfile (%s) values %s;'
     lg = RDLogger.logger()
     lg.setLevel(RDLogger.CRITICAL)
     count = 0
@@ -207,39 +221,78 @@ def readsdfiles(fname):
         while True:
             sdrecord = readnextSDfile(file)
             if sdrecord != 'EOF':
-                writedb(conn, sdrecord)
+                writedb(conn, sdrecord, sql)
                 count += 1
-                if (count % 20000 == 0):
-                    print(count)
+                if (count % 50000 == 0):
+                    print('readsdfiles records',count)
             else:
                break
 
-    conn.commit()
-    conn.close()
+    flush(conn)
     print("wrote ", count, " records")
 
 
 
-def writedb(conn, data):
+def writedb(conn, data, sql):
      """ write a SDFile record the database """
-     if  not 'XRN' in data.keys() or data['XRN'] == None or data['XRN'] == '':
-         return
+    # if  not 'XRN' in data.keys() or data['XRN'] == None or data['XRN'] == '':
+    #     return
 
-     sql = 'insert into rmc.sdfile (%s) values %s on conflict(xrn) do nothing'
      with conn.cursor() as cur:
          columns = data.keys()
          values = [data[column] for column in columns]
-         if debug:
-             print(cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))))
-         else:
-             cur.execute(sql, (AsIs(','.join(columns)), tuple(values)))
+         cmd = cur.mogrify(sql, (AsIs(','.join(columns)), tuple(values))).decode('utf-8')
+
+         h = hash(cmd)
+         if not h in lines:
+            lines.add(h)
+            insertcache.add(cmd)
+            if len(insertcache) >= CHUNKSIZE:
+                flush(conn)
+   
+def flush(conn):
+""" flush the data cache to the database """
+  if (debug):
+      print('flushed cache of ', len(insertcache) )
+
+  statement = '\n'.join(insertcache) 
+  with conn.cursor() as cur:
+      if len(statement) > 1:
+        cur.execute(statement)
+
+  conn.commit()
+  insertcache.clear()
+
 
 
 def readsdfile():
   """ read the SDFiles. This requires special functions because this is not an XML file
   """
-  #delete('rmc.sdfile')
-  path = './' + rmcversion + '/*.sdf.gz' 
+  path = './*.sdf.gz' 
   print(path)
   for filepath in glob.iglob(path):
     readsdfiles(filepath)
+
+
+
+#
+# import the set of RMC files into a postgres database.
+# this assumes that the current directory is the one with the file set.
+#
+# reads the unzipped gz file as supplied by Elsevier
+#
+
+def load():
+    readassay()
+    readcitation()
+    readdatapoint()
+    readfact()
+    readtarget()
+    readsdfile()
+
+
+initdb()
+load()
+# apply indices
+sqlfromfile('../loader/rmc.index')
+
