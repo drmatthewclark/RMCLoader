27d04f4d9bc1ccfebbc642aed2ed5379693f2ced
Matt Clark <mclark@ip-10-156-21-6.els.vpc.local>
2021-11-01 02:18:52 +0000

    import credentials

--- dbconnect.py
+++ dbconnect.py
@@ -1,19 +1,11 @@
 import psycopg2 as psql
 import os
 from subprocess import check_output
-
-host = 'localhost'
-user = 'mclark'
-password = ''
-dbase = 'mclark'
-
-
-dbname='mclark'
+from credentials import *  # import dbase, user, password, host variables
 
 def getConnection():
     conn = psql.connect(dbname=dbase, user=user, password=password, host=host)
     return conn
-
 
 def psql_cmd(command):
     env = dict(os.environ, PGPASSWORD=password)
